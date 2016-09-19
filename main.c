117c11a87adac9476578079266fe3ac646e12cf7
André Gonçalves Ohuti <ohuti@hotmail.com>
2016-09-19 14:36:18 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ char **intitialize();
 //imprime Matriz
 void printM(char **M);
 
-void movePeca(char **M, int a, int b, int c, int d);
+//Como o nome da função diz, MOVE A PEÇA e define o momento de comer a peça adversária.
+void movePeca(char **M, int i, int j, int lin, int col, char jogador);
 
 //define regras do jogo, como: evitar usar casas fora do tabuleiro, como a peça pode andar e comer.
-void step();
+int step(char **M, int i, int j, int lin, int col, char jogador);
 
 /*
 Verifica se o jogo pode ou não continuar
 caso as peças acabem ou não possam mais se mover.
 */
-//void status();
-
+//int status(char **M);
 
 /*Função que aplica a lógica do jogo, que alterna a rodada de cada jogador e
 que imprime o tabuleiro atualizado a cada rodada.
@@ -27,17 +27,10 @@ void game();
 
 int main(void){
 
-	int S;
-
 	printf("Bem vindo a Dama\n");
-	printf("Peças podem comer para trás? Se sim digite 1, se não digite 0\n");
-	scanf("%d", &S);
-	printf("Nota: o jogo funciona a base de coordenadas, selecione primeiro o numero, depois a letra\n");
+	printf("Nota: o jogo funciona a base de coordenadas, selecione primeiro o numero a esquerda, depois o numero acima\n");
 
-	if(S == 0){
-		game();
-	}/*else
-		game1();*/
+	game();
 
 	return 0;
 }
@@ -96,7 +89,6 @@ char **intitialize(){
 
 
 	return M;
-
 }
 
 void printM(char **M){
@@ -113,69 +105,118 @@ void printM(char **M){
     	printf("\n");
 
 	}
+}
+
+int step(char **M, int i, int j, int lin, int col, char jogador){
+
+	//limitando tabuleiro. -Funcionando.
+	if(lin < 0 || lin >= N || col < 0 || col >= N)
+        return 0;
+	//Checando se a peça que está na casa escolhida é a o jogador. -Funcionando.
+    if(M[i][j] != jogador)
+    	return 0;
+
+
+	//Peças Brancas.
+	//Proibindo peças de andarem para trás e para os lados. -Funcionando.
+    if(jogador == 'O'){
+    	if(lin >= i)
+    		return 0;
+
+		//Proibindo peças de andar para frete.
+    	if(col == j)
+    		return 0;
+
+    	//Permitindo avançar apenas 1 casa ou avançar 2 no caso da ação "COMER".
+    	if(lin <= i - 2 || col <= j-2 || col >= j+2){
+    		if(M[lin][col] == 'X')
+    			return 0;
+
+    		if(M[lin+1][col-1] == 'X')
+    			return 1;
+    		else if (M[lin+1][col+1] == 'X')
+    			return 1;
+    		else 
+    			return 0;
+    		if(M[lin][col] == 'X')
+    		return 0;
+    	}
+
+    	//Não permitindo peça sobrepor peça adversária.
+    	if(M[lin][col] == 'O' || M[lin][col] == 'X')
+    	    return 0;
+    }
+
+	//Peças Pretas.
+	//Proibindo peças de andarem para trás e para os lados. -Funcionando.
+    if(jogador == 'X'){
+    	if(lin <= i)
+    		return 0;
+
+		//Proibindo peças de andar para frete.
+    	if(col == j)
+    		return 0;
+
+    	//Permitindo avançar apenas 1 casa ou avançar 2 no caso da ação "COMER"
+    	if(lin <= i - 2 || col <= j-2 || col >= j+2){
+    		if(M[lin][col] == 'O')
+    			return 0;
+
+    		if(M[lin-1][col-1] == 'O')
+    			return 1;
+    		else if (M[lin-1][col+1] == 'O')
+    			return 1;
+    		else 
+    			return 0;
+    	}
+
+    	//Não permitindo peça sobrepor peça adversária.
+    	if(M[lin][col] == 'O' || M[lin][col] == 'X')
+    	    return 0;
+    }    
 
     
+
+	M[i][j] = jogador;
+	
+	return 1;
+}
+
+/*int status(char **M){
+
+
+}*/
+
+void movePeca(char **M, int i, int j, int lin, int col, char jogador){
+
+	M[lin][col] = M[i][j];
+	M[i][j] = ' ';
+
+	//Come peça.
+	if(jogador == 'O'){
+		if(lin <= i - 2 || col <= j-2 || col >= j+2){
+			if(M[lin+1][col-1] == 'X')
+				M[lin+1][col-1] = ' ';
+
+			else if(M[lin+1][col+1] == 'X')
+				M[lin+1][col+1] = ' ';
+		}
+	}
+
+	if(jogador == 'X'){
+		if(lin <= i - 2 || col <= j-2 || col >= j+2){
+			if(M[lin-1][col-1] == 'O')
+				M[lin-1][col-1] = ' ';
+
+			else if(M[lin-1][col+1] == 'O')
+				M[lin-1][col+1] = ' ';
+		}
+	}
+
+
 }
 
 
-void step(char **M, int a, int b, int c, int d,Jogador char){
-
-
-	if (Jogador == 'X' && (a <= c || b==d) )
-		//erro
-
-	if  M[c][d] != ' '
-		//erro
-
-	if (Jogador == 'O' && (a >= c || b==d)) 
-		//erro
-
-	if (b>d && Jogador == 'X')
-		if (M[a-1][b-1] != ' ' && != Jogador)
-			M[a-1][b-1] = ' '	
-
-	else if (b<d && Jogador == 'X')
-		if (M[a-1][b+1] != ' ' && != Jogador)
-			M[a-1][b+1] = ' '
-
-	
-	if (b<d && Jogador == 'O')
-		if (M[a+1][b+1] != ' ' && != Jogador)
-			M[a+1][b+1] = ' '	
-
-	else if (b>d && Jogador == 'O')
-		if (M[a+1][b-1] != ' ' && != Jogador)
-			M[a+1][b-1] = ' '			
-
-
-	
-
-	//controlar o movimento.
-
-
-
-
-
-	//controlar onde esta sendo alocada.
-
-
-	//controlar
-
-
-	//escolhe a peça que irá se mover e escolhe a casa para onde a peça vai andar.
-	
-
-}
-
-void movePeca(char **M, int a, int b, int c, int d){
-
-	step(M,a,b,c,d,)
-
-
-	M[c][d] = M[a][b];
-	M[a][b] = ' ';
-
-}
 
 void game(){
 	char **M;
@@ -199,9 +240,13 @@ void game(){
 		scanf("%d %d", &i, &j);
 		printf("escolha casa para a qual a peça devera se mover: ");
 		scanf("%d %d", &lin, &col);
-		movePeca(tabuleiro,i-1, j-1, lin-1, col-1);
-
-	
+		// se nao posso prosseguir
+        if( !step(tabuleiro, i-1, j-1, lin-1, col-1, jogador) ){
+            printf("\njogada invalida !\n");
+            continue;
+        }
+ 		movePeca(tabuleiro,i-1, j-1, lin-1, col-1, jogador);
+					
 		//alternando jogadores.
 		if(jogador == 'O')
             jogador = 'X';
@@ -210,12 +255,3 @@ void game(){
 	
 	}
 }
-
-
-
-
-
-
-
-
-
